ed51cbc98b7f895c066f6749f5fdbe27b0362e39
kyoji28 <tsukazaki@dragon.t.u-tokyo.ac.jp>
2024-12-27 14:21:32 +0900

    [Mini Quadrotor][Human Interaction]completed start, takeoff, land commands by voice recognition

--- robots/mini_quadrotor/scripts/voice_operate.py
+++ robots/mini_quadrotor/scripts/voice_operate.py
@@ -9,7 +9,7 @@ from speech_recognition_msgs.msg import SpeechRecognitionCandidates
 
 class voice_operate():
     def __init__(self):
-
+                
         # self.fly_flag = False
         # self.arm_flag = False
         
@@ -18,34 +18,32 @@ class voice_operate():
         self.land_pub = rospy.Publisher('/quadrotor/teleop_command/land',Empty,queue_size = 10)
         self.voice_text_sub = rospy.Subscriber('/speech_to_text', SpeechRecognitionCandidates, self.voice_cb)
         
-
+        
     def voice_cb(self, msg):
         print(msg.transcript[0])
-        command = msg.transcript[0]
-        confidence =  msg.confidence[0]
-
-        if "start" in command:
-            print("Start, {}".format(confidence))
+        self.command = msg.transcript[0]
+        self.confidence =  msg.confidence[0]
+        
+        if "start" in self.command:
+            print("Start, {}".format(self.confidence))
             # self.arm_flag = True
             self.start_pub.publish(Empty())
             
-        if "take" in command and "off" in command:
-            print("Takeoff, {}".format(confidence))
+        if "take" in self.command and "off" in self.command:
+            print("Takeoff, {}".format(self.confidence))
             # TODO1: pusblish takeoff command
             # self.fly_flag = True
-            self.takeoff_pub.publish((Empty())
-
+            self.takeoff_pub.publish(Empty())
+                                     
         #TODO2: other command: land, move, etc...
-        if "land" in command:
-            print("Land,{}".format(confidence))
+        if "land" in self.command:
+            print("Land,{}".format(self.confidence))
             # self.fly_flag = False
             self.land_pub.publish(Empty())
 
-
         
-
+        
 if __name__ == '__main__':
     rospy.init_node("voice_operate")
     node = voice_operate()
     rospy.spin()
-
